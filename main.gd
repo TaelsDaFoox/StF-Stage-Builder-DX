54c880bc1ecf2914cd97761ca76df4d82aa21da4
TaelsDaFoox <bazmulligan@gmail.com>
2026-04-04 19:44:38 -0700

    Floor, Pole, and Lighting Color menus
    3 new customizable things, featuring an entirely new menu for Lighting Color

--- main.gd
+++ main.gd
@@ -18,7 +18,9 @@ extends Control
 @onready var lineEditTemplate = $LineEditTemplate
 @onready var backBtn = $BackButton
 @onready var errorLog = $ErrorLog
+@onready var colorPickTemplate=$ColorPickerTemplate
 var romPath:String
+var geraldDir:String
 #var rom
 var selectedStage=-1
 enum MENUS{
@@ -26,11 +28,34 @@ enum MENUS{
 	STAGES,
 	EDITMAIN,
 	SETPATH,
-	PLATFORM
+	STAGEDATA,
+	BRIGHTNESS,
+	POSITION,
+	TEXPAL,
+	RGB,
+	MUSICID,
+	SKYCOLOR,
+	FLOOR,
+	PLATFORM,
+	POLE,
+	EXTRA,
+	OBJECTS,
+	WALLS,
+	PROPERTY,
+	DSP,
+	CLOUDS,
+	SOKO,
+	WALLHEIGHT,
+	ARENASCALE
 }
 var menu:MENUS = MENUS.MAIN
 
 func _ready() -> void:
+	var config = FileAccess.open("user://config.txt", FileAccess.READ)
+	if config:
+		romPath=config.get_line()
+		geraldDir=config.get_line()
+	
 	Global.buttonPressed.connect(btnPressed)
 	btnTemplate.visible=false
 	lineEditTemplate.visible=false
@@ -73,7 +98,7 @@ func refreshMenu():
 				var newBtn= btnTemplate.duplicate()
 				newBtn.text=mainMenuOptions[i]
 				newBtn.visible=true
-				if i<4 and !romPath:
+				if i<3 and !romPath:
 					newBtn.disabled=true
 				optionList.add_child(newBtn)
 		MENUS.SETPATH:
@@ -81,6 +106,8 @@ func refreshMenu():
 			var newBtn= lineEditTemplate.duplicate()
 			newBtn.visible=true
 			newBtn.placeholder_text="Path to rom_code1.bin"
+			if romPath:
+				newBtn.text=romPath
 			optionList.add_child(newBtn)
 			newBtn=btnTemplate.duplicate()
 			newBtn.visible=true
@@ -99,6 +126,45 @@ func refreshMenu():
 			newBtn.visible=true
 			newBtn.text="Confirm!"
 			optionList.add_child(newBtn)
+		MENUS.FLOOR:
+			header.text="Floor Model"
+			description.visible=true
+			var gotValue=ReadFromRomWithStageOffset("0x18".hex_to_int())
+			description.text="Original model ID:\n"+str(gotValue)
+			var newBtn= lineEditTemplate.duplicate()
+			newBtn.visible=true
+			newBtn.placeholder_text="New Model ID"
+			optionList.add_child(newBtn)
+			newBtn=btnTemplate.duplicate()
+			newBtn.visible=true
+			newBtn.text="Confirm!"
+			optionList.add_child(newBtn)
+		MENUS.POLE:
+			header.text="Pole Model"
+			description.visible=true
+			var gotValue=ReadFromRomWithStageOffset("0x1C".hex_to_int())
+			description.text="Original model ID:\n"+str(gotValue)
+			var newBtn= lineEditTemplate.duplicate()
+			newBtn.visible=true
+			newBtn.placeholder_text="New Model ID"
+			optionList.add_child(newBtn)
+			newBtn=btnTemplate.duplicate()
+			newBtn.visible=true
+			newBtn.text="Confirm!"
+			optionList.add_child(newBtn)
+		MENUS.RGB:
+			header.text="Lighting Color"
+			description.visible=true
+			var rgb=ReadRGBFromRomWithStageOffset("0x10".hex_to_int())
+			description.text="Original Color:\n"+str(rgb)
+			var newBtn= colorPickTemplate.duplicate()
+			newBtn.get_node("ColorPickerButton").color=Color.from_rgba8(rgb[0]*2,rgb[1]*2,rgb[2]*2,255)
+			newBtn.visible=true
+			optionList.add_child(newBtn)
+			newBtn=btnTemplate.duplicate()
+			newBtn.visible=true
+			newBtn.text="Confirm!"
+			optionList.add_child(newBtn)
 
 
 func _process(delta: float) -> void:
@@ -110,6 +176,7 @@ func _process(delta: float) -> void:
 			for i in optionList.get_children():
 				if i.is_hovered():
 					stagePreviewPic.visible=true
+					stagePreviewPic.modulate=Color.WHITE
 					if i.get_index()+1<=stagePreviewImages.size():
 						stagePreviewPic.texture=stagePreviewImages[i.get_index()]
 		MENUS.EDITMAIN:
@@ -122,6 +189,14 @@ func _process(delta: float) -> void:
 			description.text="Enter the path to rom_code1.bin in your Sonic the Fighters installation.\n\nIt should be located in your RPCS3 folder at:\n\ndev_hdd0/game/NPUB30927/USRDIR/rom/stf_rom/rom_code1.bin"
 		MENUS.PLATFORM:
 			description.visible=true
+		MENUS.FLOOR:
+			description.visible=true
+		MENUS.POLE:
+			description.visible=true
+		MENUS.RGB:
+			stagePreviewPic.visible=true
+			stagePreviewPic.texture=stagePreviewImages[selectedStage]
+			stagePreviewPic.modulate=optionList.get_node("ColorPickerTemplate/ColorPickerButton").color
 func btnPressed(idx:int):
 	sfxConfirm.play()
 	match menu:
@@ -146,13 +221,23 @@ func btnPressed(idx:int):
 				print("Error: ", FileAccess.get_open_error())
 			else:
 				rom.close()
+				var config = FileAccess.open("user://config.txt", FileAccess.WRITE)
+				config.store_line(romPath)
+				config.store_line(geraldDir)
+				config.close()
 				menu=MENUS.MAIN
 				refreshMenu()
 		MENUS.EDITMAIN:
 			match idx:
+				4:
+					menu=MENUS.RGB
+				7:
+					menu=MENUS.FLOOR
 				8:
 					menu=MENUS.PLATFORM
-					refreshMenu()
+				9:
+					menu=MENUS.POLE
+			refreshMenu()
 		MENUS.PLATFORM:
 			if optionList.get_node("LineEditTemplate").text.is_valid_int():
 				var newValue = int(optionList.get_node("LineEditTemplate").text)
@@ -166,11 +251,56 @@ func btnPressed(idx:int):
 			else:
 				errorLog.text="Invalid number!"
 				sfxBack.play()
+		MENUS.FLOOR:
+			if optionList.get_node("LineEditTemplate").text.is_valid_int():
+				var newValue = int(optionList.get_node("LineEditTemplate").text)
+				var result = WriteToRomWithStageOffset(newValue,"0x18".hex_to_int())
+				if result:
+					menu=MENUS.EDITMAIN
+					refreshMenu()
+				else:
+					errorLog.text="Something went wrong.\nIs the game still running? (It shouldn't be!)"
+					sfxBack.play()
+			else:
+				errorLog.text="Invalid number!"
+				sfxBack.play()
+		MENUS.POLE:
+			if optionList.get_node("LineEditTemplate").text.is_valid_int():
+				var newValue = int(optionList.get_node("LineEditTemplate").text)
+				var result = WriteToRomWithStageOffset(newValue,"0x1C".hex_to_int())
+				if result:
+					menu=MENUS.EDITMAIN
+					refreshMenu()
+				else:
+					errorLog.text="Something went wrong.\nIs the game still running? (It shouldn't be!)"
+					sfxBack.play()
+			else:
+				errorLog.text="Invalid number!"
+				sfxBack.play()
+		MENUS.RGB:
+			var color = optionList.get_node("ColorPickerTemplate/ColorPickerButton").color
+			var result = WriteByteArrayToRomWithStageOffset([int(color.r*128),int(color.g*128),int(color.b*128)],"0x10".hex_to_int())
+			if result:
+				menu=MENUS.EDITMAIN
+				refreshMenu()
+			else:
+				errorLog.text="Something went wrong.\nIs the game still running? (It shouldn't be!)"
+				sfxBack.play()
 func WriteToRomWithStageOffset(value:int,offset:int) -> bool:
 	if selectedStage!=-1 and romPath:
 		var rom = FileAccess.open(romPath, FileAccess.READ_WRITE)
 		rom.seek(stageRomOffsets[selectedStage].hex_to_int()+offset)
 		var didItWork = rom.store_16(value)
+		rom.close()
+		return didItWork
+	return false
+func WriteByteArrayToRomWithStageOffset(value:Array[int],offset:int) -> bool:
+	if selectedStage!=-1 and romPath:
+		var rom = FileAccess.open(romPath, FileAccess.READ_WRITE)
+		rom.seek(stageRomOffsets[selectedStage].hex_to_int()+offset)
+		var didItWork=false
+		for i in value:
+			didItWork = rom.store_8(i)
 		rom.close()
 		return didItWork
 	return false
@@ -182,6 +312,16 @@ func ReadFromRomWithStageOffset(offset:int) -> int:
 		rom.close()
 		return gotValue
 	return -1
+func ReadRGBFromRomWithStageOffset(offset:int) -> Array[int]:
+	if selectedStage!=-1 and romPath:
+		var rom = FileAccess.open(romPath, FileAccess.READ)
+		rom.seek(stageRomOffsets[selectedStage].hex_to_int()+offset)
+		var gotValue:Array[int]=[]
+		for i in 3:
+			gotValue.append(rom.get_8())
+		rom.close()
+		return gotValue
+	return []
 
 
 func _on_back_button_pressed() -> void:
@@ -195,6 +335,12 @@ func _on_back_button_pressed() -> void:
 		MENUS.SETPATH:
 			menu=MENUS.MAIN
 		MENUS.PLATFORM:
+			menu=MENUS.EDITMAIN
+		MENUS.FLOOR:
+			menu=MENUS.EDITMAIN
+		MENUS.POLE:
+			menu=MENUS.EDITMAIN
+		MENUS.RGB:
 			menu=MENUS.EDITMAIN
 	refreshMenu()
 
